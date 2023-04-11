947295e1c5462004638c3ffacf7b8c5bdf49aef2
Guy Wilson <wilson.guy@gmail.com>
2023-04-11 20:21:09 +0100

    Added clk_div to pwm setup

--- src/pwm_rp2040.c
+++ src/pwm_rp2040.c
@@ -10,15 +10,11 @@ void pwmInit() {
 
     gpio_set_function(PWM_ANEMOMETER_PIN, GPIO_FUNC_PWM);
 
-    // Find out which PWM slice is connected to GPIO 0 (it's slice 0)
     slice = pwm_gpio_to_slice_num(PWM_ANEMOMETER_PIN);
 
-    // Set period of 4 cycles (0 to 3 inclusive)
+    pwm_set_clkdiv(slice, 256.0);
     pwm_set_wrap(slice, 10849);
-    
-    // Set channel A output high for one cycle before dropping
     pwm_set_gpio_level(PWM_ANEMOMETER_PIN, 128);
     
-    // Set the PWM running
     pwm_set_enabled(slice, true);
 }
