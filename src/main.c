721132334d4d06240b2cfaa29e1296b6356a90ca
Guy Wilson <wilson.guy@gmail.com>
2023-06-04 15:14:30 +0100

    Updated logging level for debugging

--- src/main.c
+++ src/main.c
@@ -49,7 +49,7 @@ void setup(void) {
     gpio_set_function(I2C_SDA_ALT_PIN, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SLK_ALT_PIN, GPIO_FUNC_I2C);
     
-    lgOpen(uart0, LOG_LEVEL_FATAL | LOG_LEVEL_ERROR | LOG_LEVEL_STATUS);
+    lgOpen(uart0, LOG_LEVEL_FATAL | LOG_LEVEL_ERROR | LOG_LEVEL_STATUS | LOG_LEVEL_DEBUG | LOG_LEVEL_INFO);
 
 	if (initSensors(i2c0)) {
 		lgLogError("ERR: Sensor init");
